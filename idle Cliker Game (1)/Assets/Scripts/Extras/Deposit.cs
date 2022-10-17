2752ec5e34045ee278c195a998845da2d02a910e
kjs613 <ggm.kimjiseong613@gmail.com>
2022-10-17 20:31:26 +0900

    2022-10-17

--- idle Cliker Game (1)/Assets/Scripts/Extras/Deposit.cs
+++ idle Cliker Game (1)/Assets/Scripts/Extras/Deposit.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 public class Deposit : MonoBehaviour
 {
     public float CurrentGold { get; set; }
+
+    public bool canCollectGold => CurrentGold > 0;
+
     public void DepositGold(float amount)
     {
         CurrentGold += amount;
     }
+    public void RemoveGold(float amount)
+    {
+        if(amount <= CurrentGold)
+        {
+            CurrentGold -= amount;
+        }
+    }
+
+    public float CollectGold(BaseMiner miner)
+    {
+        float minerCapcity = miner.CollectCapacity - miner.CurrentGold;
+        return EvaluateAmountToCollect(minerCapcity);
+    }
+    public float EvaluateAmountToCollect(float minerCollectCapcity)
+    {
+        if(minerCollectCapcity <= CurrentGold)
+        {
+            return minerCollectCapcity;
+        }
+        return CurrentGold;
+    }
+    
 }
--- idle Cliker Game (1)/Assets/Scripts/Miner/ElevatorMiner.cs
+++ idle Cliker Game (1)/Assets/Scripts/Miner/ElevatorMiner.cs
@@ -7,6 +7,7 @@ public class ElevatorMiner : BaseMiner
     [SerializeField] private Elevator elevator;
     public Vector3 DepositLocation => new Vector3(transform.position.x, elevator.DepositLocation.position.y);
     private int _currentShaftIndex = -1;
+    private Deposit _currentShaftDeposit;
     public override void OnClick()
     {
         MoveTONextLocation();
@@ -15,18 +16,23 @@ public class ElevatorMiner : BaseMiner
     {
         _currentShaftIndex++;
         Shaft currentShaft = ShaftManager.Instance.Shafts[_currentShaftIndex];
+        _currentShaftDeposit = currentShaft.ShaftDeposit;
         Vector3 shaftDepositPos = currentShaft.DepositLocation.position;
         Vector3 fixedPos = new Vector3(transform.position.x, shaftDepositPos.y);
         MoveMiner(fixedPos);
     }
     protected override void CollectGold()
     {
-        if(_currentShaftIndex == ShaftManager.Instance.Shafts.Count - 1)
+        if(_currentShaftIndex == ShaftManager.Instance.Shafts.Count - 1 && !_currentShaftDeposit.canCollectGold)
         {
             ChangeGoal();
             MoveMiner(DepositLocation);
             _currentShaftIndex = -1;
+            return;
         }
+        float amountToCollect = _currentShaftDeposit.CollectGold(this);
+        float coolectTime = amountToCollect / CollectPerSecond;
+        StartCoroutine(IECollect(amountToCollect, coolectTime));
     }
     protected override void DepositGold()
     {
@@ -34,6 +40,26 @@ public class ElevatorMiner : BaseMiner
         {
             ChangeGoal();
             MoveTONextLocation();
+        }
+    }
+    protected override IEnumerator IECollect(float gold, float collectTime)
+    {
+        yield return new WaitForSeconds(collectTime);
+        if (CurrentGold < CollectCapacity && gold <= (CollectCapacity - CurrentGold))
+        {
+            CurrentGold += gold;
+            _currentShaftDeposit.RemoveGold(gold);
+        }
+        yield return new WaitForSeconds(0.5f);
+        if(CurrentGold < CollectCapacity &&  _currentShaftIndex != ShaftManager.Instance.Shafts.Count - 1)
+        {
+            MoveTONextLocation();
+        }
+        else
+        {
+            ChangeGoal();
+            MoveMiner(DepositLocation);
+            _currentShaftIndex = -1;
         }
     }
 }
